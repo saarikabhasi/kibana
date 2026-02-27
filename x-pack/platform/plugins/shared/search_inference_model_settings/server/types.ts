50203ee9ca828437492ccff2885c6a014ff5ab18
saarikabhasi <nairsaarika.bhasi@elastic.co>
2026-02-28 00:30:53 +0530

    del files for rename

--- x-pack/platform/plugins/shared/search_inference_model_settings/server/types.ts
+++
@@ -1,9 +0,0 @@
-import type { FeaturesPluginSetup } from '@kbn/features-plugin/server';
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface SearchInferenceModelSettingsPluginSetup {}
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface SearchInferenceModelSettingsPluginStart {}
-export interface SearchInferenceModelSettingsPluginStartDependencies {
-  features: FeaturesPluginSetup;
-}
